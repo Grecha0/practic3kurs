d2c491bf9e83c57cdb02000ec8141d7ac3b6b477
Roman Serguntsov <Serguntsov>
2024-11-14 18:59:50 +0300

    function fade()

--- main.py
+++ main.py
@@ -303,13 +303,24 @@ def new_game_against_bot(difficulty):
     pygame.quit()
 
 def fade():
-    fade_surface = pygame.Surface((WIDTH, HEIGHT))
-    fade_surface.fill((0, 0, 0))
-    for alpha in range(0, 300):
-        fade_surface.set_alpha(alpha)
+    running = True
+    fade_alpha = 0  # Уровень прозрачности для анимации
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        # Анимация затухания текущего экрана
+        fade_surface = pygame.Surface((WIDTH, HEIGHT))
+        fade_surface.fill((0, 0, 0))
+        fade_surface.set_alpha(fade_alpha)
         screen.blit(fade_surface, (0, 0))
-        pygame.display.update()
-        pygame.time.delay(1)
+        # Увеличение уровня прозрачности
+        fade_alpha += 5
+        if fade_alpha >= 105:
+            fade_alpha = 255
+            running = False
+        pygame.display.flip()
+        clock.tick(MAX_FPS)  # Ограничение FPS
 
 def get_row_col_from_mouse(pos):
     x, y = pos
